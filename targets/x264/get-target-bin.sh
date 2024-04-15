04c8a01a36f3edbc3b871bd6ac0761cc4551cb3e
Pranjal Singh <pranjal.singh@dartmouth.edu>
2024-04-15 15:07:29 -0400

    Fix image+container+directory names

--- targets/x264/get-target-bin.sh
+++ targets/x264/get-target-bin.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-rm -rf aflplusplus_docker/targets \
-    && mkdir aflplusplus_docker/targets \
-    && docker build . -t target-base \
-    && docker run -d --name build-harnesses target-base sleep infinity \
-    && docker cp build-harnesses:/app/targets ./aflplusplus_docker \
+rm -rf aflplusplus_docker/harnesses \
+    && docker build . -t harness-base \
+    && docker run -d --name build-harnesses harness-base sleep infinity \
+    && docker cp build-harnesses:/app/harnesses ./aflplusplus_docker \
     && docker kill build-harnesses \
     && docker rm build-harnesses
